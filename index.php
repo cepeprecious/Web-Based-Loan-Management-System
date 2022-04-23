c3942bdff335374bc066a0f23683d1c24cd3aa17
yamachine1993 <cepeprecious00@gmail.com>
2022-04-23 08:11:01 +0800

    Added few changes

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style-1.css">
+    <title>Landing Page</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="title">Welcome!</div>
+        <div class="sub-wrapper">
+            <div class="subtitle">Please choose where you belong.</div>
+            <div class="field">
+                <a href="User/login-user.php">
+                    <input type="submit" value="User">
+                </a>
+            </div>
+            <div class="field">
+                <input type="submit" value="Admin">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
